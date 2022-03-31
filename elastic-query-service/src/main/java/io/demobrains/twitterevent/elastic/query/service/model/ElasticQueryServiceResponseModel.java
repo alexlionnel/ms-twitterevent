1b8b4541e82d5ffb81187aab051c36cf40fa7096
Alex SIEYADJEU <alex.sieyadjeu@externe.maif.fr>
2022-03-31 18:19:50 +0200

    version media content negotiation version

--- elastic-query-service/src/main/java/io/demobrains/twitterevent/elastic/query/service/model/ElasticQueryServiceResponseModel.java
+++ elastic-query-service/src/main/java/io/demobrains/twitterevent/elastic/query/service/model/ElasticQueryServiceResponseModel.java
@@ -3,11 +3,13 @@ package io.demobrains.twitterevent.elastic.query.service.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.time.ZonedDateTime;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Builder
 @NoArgsConstructor
