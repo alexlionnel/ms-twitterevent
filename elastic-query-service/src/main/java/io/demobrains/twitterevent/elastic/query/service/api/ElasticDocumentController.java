3e7355752379eba97c3b313b4b6a90c01de2c5ef
alexlionnel <alexlionnel@yahoo.fr>
2022-04-01 08:01:58 +0200

    version media negotiation (#2)
    Co-authored-by: Alex SIEYADJEU <alex.sieyadjeu@externe.maif.fr>

--- elastic-query-service/src/main/java/io/demobrains/twitterevent/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/io/demobrains/twitterevent/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,7 @@ package io.demobrains.twitterevent.elastic.query.service.api;
 import io.demobrains.twitterevent.elastic.query.service.business.ElasticQueryService;
 import io.demobrains.twitterevent.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import io.demobrains.twitterevent.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import io.demobrains.twitterevent.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/documents")
+@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 @RequiredArgsConstructor
 @Slf4j
 public class ElasticDocumentController {
@@ -39,10 +40,29 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(elasticQueryServiceResponseModel);
     }
 
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
+    public ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentByIdV2(@PathVariable @NotEmpty String id) {
+        ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = elasticQueryService.getDocumentById(id);
+        ElasticQueryServiceResponseModelV2 responseModelV2 = getV2Model(elasticQueryServiceResponseModel);
+        log.info("Elasticsearch returned document with id {}", id);
+        return ResponseEntity.ok(responseModelV2);
+    }
+
     @PostMapping("/text")
     public ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentsByText(@RequestBody @Valid ElasticQueryServiceRequestModel elasticQueryServiceRequestModel) {
         List<ElasticQueryServiceResponseModel> response = elasticQueryService.getDocumentByText(elasticQueryServiceRequestModel.getText());
         log.info("Elasticsearch returned {} of documents", response.size());
         return ResponseEntity.ok(response);
+    }
+
+    private ElasticQueryServiceResponseModelV2 getV2Model(ElasticQueryServiceResponseModel responseModel) {
+        ElasticQueryServiceResponseModelV2 responseModelV2 = ElasticQueryServiceResponseModelV2.builder()
+                .id(Long.parseLong(responseModel.getId()))
+                .userId(responseModel.getUserId())
+                .text(responseModel.getText())
+                .textV2("version 2 text")
+                .build();
+        responseModelV2.add(responseModel.getLinks());
+        return responseModelV2;
     }
 }
---
+++ elastic-query-service/src/main/java/io/demobrains/twitterevent/elastic/query/service/model/ElasticQueryServiceResponseModelV2.java
@@ -0,0 +1,20 @@
+package io.demobrains.twitterevent.elastic.query.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElasticQueryServiceResponseModelV2 extends RepresentationModel<ElasticQueryServiceResponseModelV2> {
+
+    private Long id;
+    private String text;
+    private String textV2;
+    private Long userId;
+
+}
